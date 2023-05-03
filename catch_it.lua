f69a3d4516dd0a655b13c7e0bc53c5836cf5bbb4
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2023-05-03 15:49:21 +0200

    Implemented univoque controller for robot, now they play but the simuletion don't stop

--- catch_it.lua
+++ catch_it.lua
@@ -10,16 +10,103 @@ left_v = 0
 right_v = 0
 local L = robot.wheels.axis_length
 local vector = require "vector"
+local enemy = "fb1"
 
 --[[ This function is executed every time you press the 'execute'
      button ]]
 function init()
-	reset()
+    reset()
 end
 
+--[[ This function is executed at each time step
+     It must contain the logic of your controller ]]
+function step()
+	-- Set the robot led on if it is close to the light
+	n_steps = n_steps + 1
+    if robot.id == enemy then
+        robot.leds.set_all_colors("red")
+        robot.range_and_bearing.set_data(1, 1)
 
+        vec_catch = vector_catch_it()
+        vec_catch.length = 5
 
--- This function set the velocity of the wheels to a random value
+        vec = vector_avoid_obstacles()
+        vec.length = vec.length * 10
+        vec = vector.vec2_polar_sum(vec, vec_catch)
+        
+        if vec.length > 0 then
+            log("vec.length = " .. vec.length .. " vec.angle = " .. vec.angle)
+            setVelocity(from_vector_to_velocities(vec))
+        else
+            goRandom()
+        end
+    else
+        robot.leds.set_all_colors("green")
+        robot.range_and_bearing.set_data(1, 0)
+        
+        vec_get_out = vector_get_out()
+        vec_get_out.length = 5
+        
+        vec = vector_avoid_obstacles()
+        vec.length = vec.length * 10
+        vec = vector.vec2_polar_sum(vec, vec_get_out)
+        
+        if vec.length > 0 then
+            log("vec.length = " .. vec.length .. " vec.angle = " .. vec.angle)
+            setVelocity(from_vector_to_velocities(vec))
+        else
+            goRandom()
+        end
+    end
+
+	-- Log the stats of the robot
+	logStats()
+end
+
+function vector_catch_it()
+	vec = {length = 0, angle = 0}
+    if robot.range_and_bearing[1] ~= nil then
+        vec.length = robot.range_and_bearing[1].range
+        vec.angle = robot.range_and_bearing[1].horizontal_bearing
+    end
+    return vec
+end
+
+function vector_get_out()
+	vec = {length = 0, angle = 0}
+    if robot.range_and_bearing[1] ~= nil then
+        vec.length = robot.range_and_bearing[1].range
+        vec.angle = robot.range_and_bearing[1].horizontal_bearing
+        if vec.angle > 0 then
+            vec.angle = vec.angle - math.pi
+        else
+            vec.angle = vec.angle + math.pi
+        end
+    end
+	return vec
+end
+
+function vector_avoid_obstacles()
+	vec = {length = 0, angle = 0}
+	for i=1,#robot.proximity do
+		ang = robot.proximity[i].angle
+		if ang > 0 then
+			ang = ang - math.pi
+		else
+			ang = ang + math.pi
+		end
+		vec = vector.vec2_polar_sum(vec, {length = robot.proximity[i].value, angle = ang})
+	end
+	return vec
+end
+
+function from_vector_to_velocities(vec)
+	local vel = { left = 0, right = 0}
+	vel.left = vec.length  + (-L/2) * vec.angle
+	vel.right = vec.length + (L/2) * vec.angle
+	return vel
+end
+
 function goRandom()
 	vel = {left = 0, right = 0}
 	vel.left = robot.random.uniform(0,MAX_VELOCITY)
@@ -31,81 +118,19 @@ function setVelocity(vel)
 	robot.wheels.set_velocity(vel.left,vel.right)
 end
 
-function vector_catch_it()
-	vec = {length = 0, angle = 0}
-	return vec;
-end
-
-function vector_get_out()
-	vec = {length = 0, angle = 0}
-	return vec;
-end
-
-function from_vector_to_velocities(vec)
-	local vel = { left = 0, right = 0}
-	vel.left = vec.length  + (-L/2) * vec.angle
-	vel.right = vec.length + (L/2) * vec.angle
-	return vel
-end
-
--- This method print the stats of the robot
 function logStats()
-	-- log("robot.position.x = " .. robot.positioning.position.x)
-	-- log("robot.position.y = " .. robot.positioning.position.y)
-	-- log("robot.position.z = " .. robot.positioning.position.z)
-	
-	
-	-- light_front = robot.light[1].value + robot.light[24].value
-	-- log("robot.light_front = " .. light_front)
-
-	-- -- Search for the reading with the highest value
-	-- value = -1 -- highest value found so far
-	-- idx = -1   -- index of the highest value
-	-- for i=1,#robot.proximity do
-	-- 	idx = i
-	-- 	value = robot.proximity[i].value
-	-- 	angle = robot.proximity[i].angle
-	-- 	log("robot proximity sensor: " .. idx .. "------" .. value .. "------" .. angle)
-	-- end
-	
-
-
-	-- vector = vector_avoid_obstacles()
-	-- log("avoid_obstacles_vector_length: " .. vector.length)
-	-- log("avoid_obstacles_vector_angle: " .. vector.angle)
+    --log("robot.range_and_bearing[1].data[1] = " .. robot.range_and_bearing[1].data[1])
+    --log("robot.range_and_bearing[1].horizontal_bearing = " .. robot.range_and_bearing[1].horizontal_bearing)
+    --log("robot.range_and_bearing[1].range = " .. robot.range_and_bearing[1].range)
 end
-
-
---[[ This function is executed at each time step
-     It must contain the logic of your controller ]]
-function step()
-	-- Set the robot led on if it is close to the light
-	n_steps = n_steps + 1
-
-	-- Set the velocity of the wheels
-	vec = vector.vec2_polar_sum(vector_avoid_obstacles(), vector_phototaxis())
-	vel = from_vector_to_velocities(vec)
-	if vel.left == 0 and vel.right == 0 then
-		goRandom()
-	else
-		setVelocity(vel)
-	end
-	
-	-- Log the stats of the robot
-	logStats()
-
-end
-
 
 --[[ This function is executed every time you press the 'reset'
  button in the GUI. It is supposed to restore the state
  of the controller to whatever it was right after init() was
  called. The state of sensors and actuators is reset
  automatically by ARGoS. ]]
- function reset()
-    left_v = robot.random.uniform(0,MAX_VELOCITY)
-    right_v = robot.random.uniform(0,MAX_VELOCITY)
-    robot.wheels.set_velocity(left_v,right_v)
+function reset()
+    goRandom()
     n_steps = 0
     robot.leds.set_all_colors("black")
 end
